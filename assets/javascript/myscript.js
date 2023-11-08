c3920f3d104d93bc20bfe0573f6bf59d471c2fbb
unknown <kulsumadust15@gmail.com>
2023-11-08 13:31:36 +0600

    meaning full command

---
+++ assets/javascript/myscript.js
@@ -0,0 +1,8 @@
+// navbar scroll top 
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
+//   navbar scroll end 
